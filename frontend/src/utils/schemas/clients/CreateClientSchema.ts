fe83e421da0270c87a58da980d07cc04efb9ba35
N1NHOS0 <renanpgranzotti@gmail.com>
2024-08-06 11:58:10 -0300

    Added coerce in CreateClientSchema.ts

--- frontend/src/utils/schemas/clients/CreateClientSchema.ts
+++ frontend/src/utils/schemas/clients/CreateClientSchema.ts
@@ -5,14 +5,14 @@ export const CreateClientFormSchema = z.object({
   name: z.string(),
   fantasy: z.string(),
   address: z.string(),
-  number: z.string(),
+  number: z.coerce.number(),
   complement: z.string(),
-  cep: z.string(),
+  cep: z.coerce.number(),
   district: z.string(),
   city: z.string(),
   state: z.string().min(2).max(2),
   email: z.string().email(),
-  phone: z.string(),
+  phone: z.coerce.number(),
 });
 
 export type CreateClientFormType = z.infer<typeof CreateClientFormSchema>;
